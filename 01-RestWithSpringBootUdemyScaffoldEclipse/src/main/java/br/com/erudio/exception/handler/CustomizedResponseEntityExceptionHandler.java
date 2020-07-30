205f89f55851329f7f99f819bc25bf6ebc7f1804
DanielZat <dnl.zat@gmail.com>
2020-07-30 14:43:55 -0300

    adicionada integração com mysql e implementado padrão de projeto Value Object

--- 01-RestWithSpringBootUdemyScaffoldEclipse/src/main/java/br/com/erudio/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ 01-RestWithSpringBootUdemyScaffoldEclipse/src/main/java/br/com/erudio/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.erudio.exception.ExceptionResponse;
+import br.com.erudio.exception.ResourceNotFoundException;
 import br.com.erudio.exception.UnsuportedMathOperationExeception;
 
 @ControllerAdvice
@@ -31,5 +32,13 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request) {
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 }
---
+++ 01-RestWithSpringBootUdemyScaffoldEclipse/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,61 @@
+package br.com.erudio.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.converter.DozerConverter;
+import br.com.erudio.data.model.Person;
+import br.com.erudio.data.vo.PersonVO;
+import br.com.erudio.exception.ResourceNotFoundException;
+import br.com.erudio.repository.PersonRepository;
+
+@Service
+public class PersonServices {
+
+	@Autowired
+	PersonRepository personRepository;
+		
+	public PersonVO create(PersonVO personVO) {
+		
+		var entity = DozerConverter.parseObject(personVO, Person.class); 
+		
+		var vo = DozerConverter.parseObject(personRepository.save(entity), PersonVO.class); 
+		
+		return vo;
+	}
+	
+	public PersonVO update(PersonVO person) {
+		
+		var entity = personRepository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		var vo = DozerConverter.parseObject(personRepository.save(entity), PersonVO.class); 
+		
+		return vo;
+	}
+	
+	public void delete(Long id) {
+		
+		Person entity = personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
+	
+		personRepository.delete(entity);
+	}
+	
+	public PersonVO findById(Long id) {
+		
+		var entity = personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
+		
+		return DozerConverter.parseObject(entity, PersonVO.class);
+	}
+	
+	public List<PersonVO> findAll(){
+		
+		return DozerConverter.parseListObjects(personRepository.findAll(), PersonVO.class);
+	}	
+}
